e123e2e5a6d88b8fdd75f07ec826d542ac60291e
Robin Kumar <robin.kumar@student.fh-kiel.de>
2019-03-06 14:24:43 +0100

    int to roman numerals code logic is updated.

--- int_to_roman_numerals.py
+++ int_to_roman_numerals.py
@@ -1,6 +1,5 @@
 class IntToRomans:
     def int_to_romans(self, n):
-        #romans = {1:'I', 2:'II', 3:'III', 4:'IV', 5:'V', 6:'VI', 7:'VII', 8:'VIII', 9:'IX', 10:'X' }
         
         ''' 
             Please change the value of 'n' that you want to see as a Roman Numeral at the end of this program
